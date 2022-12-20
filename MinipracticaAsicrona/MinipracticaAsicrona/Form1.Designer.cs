1142b56e40cbfe1c78c9f5b5e9c83d86cc957767
Juan <71831893+JuanRamirezHuerta@users.noreply.github.com>
2022-12-20 00:35:28 -0600

    Cargando archivos
    Se cargaron todos los archivos para la mini-practica asicrona

---
+++ MinipracticaAsicrona/MinipracticaAsicrona/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace MinipracticaAsicrona
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cargar el archivo XML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cargar datos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(62, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 51);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ordenamiento Burbuja Ascedente";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(62, 210);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 53);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Ordenamiento Shell Ascedente";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 281);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 60);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Ordenamiento con metodo Sort Ascedente en el gridview burbuja";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(234, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(260, 391);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(517, 36);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(260, 391);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(306, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Ordenamiento Burbuja";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(593, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Ordenamiento Shell";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(110, 281);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(101, 60);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Ordenamiento con metodo Sort Descendente en el gridview burbuja";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 347);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(101, 60);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Ordenamiento con metodo Sort Ascedente en el gridview shell";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(110, 347);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(101, 60);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Ordenamiento con metodo Sort Ascedente en el gridview shell";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Juan Ramirez Huerta Grupo: 10° C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MinipracticaAsicrona/MinipracticaAsicrona/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinipracticaAsicrona
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataSet contenedor = new DataSet(); // Aqui se manda todos los datos del xml para el gridview 1
+        DataSet contenedor2 = new DataSet();// Aqui se manda todos los datos del xml para el gridview 2
+        DataTable cadenas = new DataTable(); //Se crea un campo para entrar las propiedades de un datatable de burbuja
+        DataTable cadenas2 = new DataTable();//Se crea un campo para entrar las propiedades de un datatable de shell
+
+        private void button1_Click(object sender, EventArgs e) //Boton para cargar los archivos
+        {
+            contenedor.ReadXml(@"C:\Users\rhlb-\source\repos\MinipracticaAsicrona\MinipracticaAsicrona\ArchivoXML\datos.xml");// Ruta donde esta el archivo xml para el contendor 1
+            contenedor2.ReadXml(@"C:\Users\rhlb-\source\repos\MinipracticaAsicrona\MinipracticaAsicrona\ArchivoXML\datos.xml");// Ruta donde esta el archivo xml para el contendor 2
+            MessageBox.Show("Archivos cargados");//Mensaje que ya esta cargado los archivos
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Boton para que cargue los datos y se almacene en los contenedores y visualizarlo en los gridviews
+        {
+                dataGridView1.DataSource = (contenedor.Tables[0]);
+                MessageBox.Show("Tiene " + contenedor.Tables[0].Rows.Count + " total de registros" + " para el datagridview 1");
+
+                dataGridView2.DataSource = (contenedor2.Tables[0]);
+                MessageBox.Show("Tiene " + contenedor2.Tables[0].Rows.Count + " total de registros" + " para el datagridview 2");
+        }
+
+        private void button3_Click(object sender, EventArgs e) //Boton para ejecutar el metodo burbuja de forma ascedente
+        {
+            button3.Enabled = false;
+            Thread hilo = new Thread(delegate () //Se crea un hilo para trabajar forma asicrona para el metodo burbuja
+            {
+                burbujaAsc();
+                if (dataGridView1.InvokeRequired) 
+                {
+                    dataGridView1.Invoke(new MethodInvoker(delegate
+                    {
+                        MessageBox.Show("Termino el procedimiento burbuja");
+                    }));
+                }
+                if (button3.InvokeRequired)
+                {
+                    button3.Invoke(new MethodInvoker(delegate
+                    {
+                        button3.Enabled = true;
+                    }
+                    ));
+                }
+            }
+            );
+            hilo.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e) //Boton para ejecutar el metodo shell de forma ascendente
+        {
+            button4.Enabled = false;
+           
+            Thread hilo = new Thread(delegate ()//Se crea un hilo para trabajar forma asicrona para el metodo shell
+            {
+                Shell();
+                if (dataGridView2.InvokeRequired)
+                {
+                    dataGridView2.Invoke(new MethodInvoker(delegate
+                    {
+                        MessageBox.Show("Termino el procedimiento Shell");
+                    }));
+                }
+                if (button4.InvokeRequired)
+                {
+                    button4.Invoke(new MethodInvoker(delegate
+                    {
+                        button4.Enabled = true;
+                    }
+                    ));
+                }
+            }
+                );
+            hilo.Start();
+        }
+
+
+        public void burbujaAsc() //Metodo burbuja 
+        {
+            cadenas = contenedor.Tables[0];//se pone el campo "cadenas" para que agarra los datos del contenedor
+            int i = 0, indexer = 0;
+            for (i = 0; i < cadenas.Rows.Count; i++)
+            {
+                for (indexer = 0; indexer < cadenas.Rows.Count - 1; indexer++)
+                {
+                    int indprox = indexer + 1;
+                    if ((cadenas.Rows[indexer]["descripcion"].ToString().Trim()).CompareTo(cadenas.Rows[indprox]["descripcion"].ToString().Trim()) > 0)
+                    {
+                        string desc = cadenas.Rows[indexer]["descripcion"].ToString();
+                        string sat = cadenas.Rows[indexer]["sat_unimed"].ToString();
+                        string sim = cadenas.Rows[indexer]["simbolo"].ToString();
+                        cadenas.Rows[indexer]["descripcion"] = cadenas.Rows[indprox]["descripcion"];
+                        cadenas.Rows[indexer]["sat_unimed"] = cadenas.Rows[indprox]["sat_unimed"];
+                        cadenas.Rows[indexer]["simbolo"] = cadenas.Rows[indprox]["simbolo"];
+                        cadenas.Rows[indprox]["descripcion"] = desc;
+                        cadenas.Rows[indprox]["sat_unimed"] = sat;
+                        cadenas.Rows[indprox]["simbolo"] = sim;
+                    }
+                }
+
+            }
+
+            dataGridView1.DataSource = cadenas;
+
+        }
+
+        public void Shell() //Metodo Shell
+        {
+            cadenas2 = contenedor2.Tables[0];//se pone el campo "cadenas2" para que agarra los datos del contenedor2
+            int pum = 0;
+            int x = 0;
+
+            int e = 0;
+            pum = cadenas2.Rows.Count;
+            while (pum > 0)
+            {
+                x = 1;
+                while (x != 0)
+                {
+                    x = 0;
+                    e = 1;
+                    while (e <= (cadenas2.Rows.Count - pum))
+                    {
+                        if ((cadenas2.Rows[e - 1]["descripcion"].ToString().Trim().CompareTo((cadenas2.Rows[e - 1 + pum]["descripcion"].ToString().Trim())) > 0))
+                        {
+                            string aux = cadenas2.Rows[(e - 1) + pum]["descripcion"].ToString();
+                            string auxi1 = cadenas2.Rows[(e - 1) + pum]["sat_unimed"].ToString();
+                            string auxi2 = cadenas2.Rows[(e - 1) + pum]["simbolo"].ToString();
+                            cadenas2.Rows[(e - 1) + pum]["descripcion"] = cadenas2.Rows[e - 1]["descripcion"].ToString();
+                            cadenas2.Rows[(e - 1) + pum]["sat_unimed"] = cadenas2.Rows[e - 1]["sat_unimed"].ToString();
+                            cadenas2.Rows[(e - 1) + pum]["simbolo"] = cadenas2.Rows[e - 1]["simbolo"].ToString();
+                            cadenas2.Rows[(e - 1)]["descripcion"] = aux;
+                            cadenas2.Rows[(e - 1)]["sat_unimed"] = auxi1;
+                            cadenas2.Rows[(e - 1)]["simbolo"] = auxi2;
+                            x = 1;
+                        }
+                        e++;
+                    }
+                }
+                pum = pum / 2;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) //Manera de ordenar el gridview con el metodo Sort
+        {
+            if(button3.Enabled == false)
+            {
+                MessageBox.Show("Esperar el proceso asicrono de burbuja que acabe");
+            }
+            else
+            {
+                dataGridView1.Sort(dataGridView1.Columns[1], ListSortDirection.Ascending);
+             
+            }
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e)//Manera de ordenar el gridview con el metodo Sort
+        {
+            
+            if (button3.Enabled == false)
+            {
+                MessageBox.Show("Esperar el proceso asicrono de burbuja que acabe");
+            }
+            else
+            {
+                
+                dataGridView1.Sort(dataGridView1.Columns[1], ListSortDirection.Descending);
+                
+            }
+       
+        }
+
+        private void button7_Click(object sender, EventArgs e)//Manera de ordenar el gridview con el metodo Sort
+        {
+            if (button4.Enabled == false)
+            {
+                MessageBox.Show("Esperar el proceso asicrono de shell que acabe");
+            }
+            else
+            {
+                dataGridView2.Sort(dataGridView2.Columns[1], ListSortDirection.Ascending);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)//Manera de ordenar el gridview con el metodo Sort
+        {
+            if (button4.Enabled == false)
+            {
+                MessageBox.Show("Esperar el proceso asicrono de shell que acabe");
+            }
+            else
+            {
+                dataGridView2.Sort(dataGridView2.Columns[1], ListSortDirection.Descending);
+            }
+        }
+
+        
+    }
+}
